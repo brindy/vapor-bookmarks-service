a16b6e0d754b84433938c0229ff535b4c322f384
Chris Brind <brindy@duckduckgo.com>
2018-06-06 18:04:54 +0100

    take posted json and insert in to bookmarks object

--- Sources/App/Controllers/BookmarksController.swift
+++ Sources/App/Controllers/BookmarksController.swift
@@ -9,11 +9,25 @@ final class BookmarksController {
     }
 
     func create(_ req: Request) throws -> Future<String> {
-        return try req.content.decode(Bookmarks.self).flatMap { bookmarks in
-            return bookmarks.save(on: req)
-        }.map { bookmarks in
+        guard let data = req.http.body.data else {
+            throw Abort(.badRequest, reason: "No body")
+        }
+        
+        guard (try? JSONSerialization.jsonObject(with: data)) != nil else {
+            throw Abort(.badRequest, reason: "Invalid JSON")
+        }
+
+        // TODO test the json contains valid elements
+        
+        guard let bookmarksJson = String(data: data, encoding: .utf8) else {
+            throw Abort(.badRequest, reason: "Body was not utf8 encoded")
+        }
+
+        let bookmarks = Bookmarks(id: nil, json: bookmarksJson)
+        return bookmarks.save(on: req).map { bookmarks in
             return bookmarks.id!.uuidString
         }
+        
     }
 
 }
